b07fe07e8e79506e1f525678e8a339f9793379e8
de.toolcreator <de.toolcreator@gmail.com>
2020-10-10 13:37:52 +0200

    Update README

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 WPA2 password generator.
 
-- generated passwords are always 63 characters long (allowed maximum)
 - each charachter is drawn randomly from the set of allowed characters
 - random number generation is cryptographically secure by using [`getrandom`](https://man7.org/linux/man-pages/man2/getrandom.2.html)
 - linux only (for now)
@@ -25,4 +24,6 @@ $ make wpa2pg
 cc -O3 -Wall -o wpa2pg wpa2pg.c
 $ ./wpa2pg
 2jF/?g.z*U&tYakCWR/-,*^6o9mnGHkX^$BnOt~0FWc+_y6-=OAv$5pcyH@L~!T
+$ ./wpa2pg 8
+t~r_8#Uz
 ```
--- wpa2pg.c
+++ wpa2pg.c
@@ -27,24 +27,23 @@ size_t get_pwd_str_size(int argc, char *argv[]) {
 
 char get_random_char(void) {
   const char ALLOWED_CHARS[] = {
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '!', '?', '*', '#', '_', '.', '-', ',', '$', '%', '&', '/', '^', '@', '~', '=', '+', ':'
-  };
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      '!', '?', '*', '#', '_', '.', '-', ',', '$', '%', '&', '/', '^', '@', '~', '=', '+', ':'};
   const size_t NUM_ALLOWED_CHARS = sizeof(ALLOWED_CHARS) / sizeof(ALLOWED_CHARS[0]);
 
   uint8_t idx = NUM_ALLOWED_CHARS;
   do {
     getrandom(&idx, 1, GRND_NONBLOCK);
-  } while(idx >= NUM_ALLOWED_CHARS);
+  } while (idx >= NUM_ALLOWED_CHARS);
 
   return ALLOWED_CHARS[idx];
 }
 
 void generate_password(char *buffer, size_t buffer_size) {
   buffer_size -= 1;
-  for(unsigned int i = 0; i < buffer_size; ++i) {
+  for (unsigned int i = 0; i < buffer_size; ++i) {
     buffer[i] = get_random_char();
   }
   buffer[buffer_size] = '\0';
