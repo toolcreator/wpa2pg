aec7b6d096b12a4521a68d92b78f5a00555aa60e
de.toolcreator <de.toolcreator@gmail.com>
2023-10-05 20:07:36 +0200

    Add option to exclude arbitrary characters

--- src/wpa2pg.c
+++ src/wpa2pg.c
@@ -13,6 +13,7 @@ typedef struct {
   bool exclude_special;
   bool exclude_ambiguous;
   bool exclude_capital;
+  char *exclude_extra;
 } pwd_props_t;
 
 typedef struct {
@@ -24,15 +25,17 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state);
 char get_random_char(const pwd_props_t *props);
 void generate_password(char *buffer, size_t buffer_size,
                        const pwd_props_t *props);
+void freePtr(void **p);
 
-const char * argp_program_version = "wpa2pg 1.3.0";
-const char * argp_program_bug_address = "<de.toolcreator@gmail.com>";
+const char *argp_program_version = "wpa2pg 1.4.0";
+const char *argp_program_bug_address = "<de.toolcreator@gmail.com>";
 static const char doc[] = "WPA2 password generator";
 static const char args_doc[] = "LENGTH";
 static const struct argp_option options[] = {
     {"exclude-special", 's', 0, 0, "Exclude special characters"},
     {"exclude-ambiguous", 'a', 0, 0, "Exclude ambiguous characters"},
     {"exclude-capital", 'c', 0, 0, "Exclude capital letters"},
+    {"exclude-extra", 'e', "CHAR_STRING", 0, "More characters to exclude"},
     {0}};
 static const struct argp argp = {options, parse_opt, args_doc, doc};
 
@@ -40,7 +43,8 @@ int main(int argc, char *argv[]) {
   arguments_t args = {.length = 63,
                       .pwd_props = {.exclude_special = false,
                                     .exclude_ambiguous = false,
-                                    .exclude_capital = false}};
+                                    .exclude_capital = false,
+                                    .exclude_extra = NULL}};
   argp_parse(&argp, argc, argv, 0, 0, &args);
 
   const size_t pwd_str_size = args.length + 1;
@@ -49,11 +53,14 @@ int main(int argc, char *argv[]) {
     generate_password(pwd_str, pwd_str_size, &args.pwd_props);
     printf("%s\n", pwd_str);
 
-    free(pwd_str);
-    pwd_str = NULL;
+    freePtr((void **)&pwd_str);
   } else {
-    printf("Failed to allocate memory for a password of length %lu\n",
-           pwd_str_size - 1);
+    fprintf(stderr, "Failed to allocate memory for a password of length %lu\n",
+            pwd_str_size - 1);
+  }
+
+  if (args.pwd_props.exclude_extra != NULL) {
+    freePtr((void **)&args.pwd_props.exclude_extra);
   }
 
   return 0;
@@ -63,34 +70,46 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   arguments_t *arguments = state->input;
 
   switch (key) {
-    case 's':
-      arguments->pwd_props.exclude_special = true;
-      break;
-    case 'a':
-      arguments->pwd_props.exclude_ambiguous = true;
-      break;
-    case 'c':
-      arguments->pwd_props.exclude_capital = true;
-      break;
-    case ARGP_KEY_ARG:
-      if (state->arg_num >= 1) {
-        argp_usage(state);
-      }
+  case 's':
+    arguments->pwd_props.exclude_special = true;
+    break;
+  case 'a':
+    arguments->pwd_props.exclude_ambiguous = true;
+    break;
+  case 'c':
+    arguments->pwd_props.exclude_capital = true;
+    break;
+  case 'e': {
+    char **extra = &arguments->pwd_props.exclude_extra;
+    *extra = calloc(strlen(arg) + 1, sizeof(char));
+    if (extra != NULL) {
+      strcpy(*extra, arg);
+    } else {
+      fprintf(stderr,
+              "Failed to allocate memory for extra excluded characters\n");
+      return 1;
+    }
+    break;
+  }
+  case ARGP_KEY_ARG:
+    if (state->arg_num >= 1) {
+      argp_usage(state);
+    }
 
-      const int parsed_length = atoi(arg);
-      if (parsed_length > 0) {
-        arguments->length = parsed_length;
-      } else {
-        argp_usage(state);
-      }
+    const int parsed_length = atoi(arg);
+    if (parsed_length > 0) {
+      arguments->length = parsed_length;
+    } else {
+      argp_usage(state);
+    }
 
-      break;
+    break;
 
-    case ARGP_KEY_END:
-      break;
+  case ARGP_KEY_END:
+    break;
 
-    default:
-      return ARGP_ERR_UNKNOWN;
+  default:
+    return ARGP_ERR_UNKNOWN;
   }
   return 0;
 }
@@ -142,11 +161,24 @@ char get_random_char(const pwd_props_t *props) {
   allowed = cll_begin(allowed);
   const size_t allowed_chars_count = cll_length(allowed);
   char idx = allowed_chars_count;
+
+  static const unsigned int MAX_ITERATIONS = 10000;
+  unsigned int i = 0;
   do {
     getrandom(&idx, sizeof(idx), GRND_NONBLOCK);
-  } while (idx >= allowed_chars_count);
+  } while ((idx >= allowed_chars_count ||
+            (props->exclude_extra != NULL
+                 ? strchr(props->exclude_extra, cll_index(allowed, idx)) != NULL
+                 : false)) &&
+           ++i < MAX_ITERATIONS);
 
-  return cll_index(allowed, idx);
+  if (i == MAX_ITERATIONS) {
+    printf("Character selection is too restrictive, giving up\n");
+    exit(EXIT_FAILURE);
+  }
+
+  const char c = cll_index(allowed, idx);
+  return c;
 }
 
 void generate_password(char *buffer, size_t buffer_size,
@@ -156,4 +188,9 @@ void generate_password(char *buffer, size_t buffer_size,
     buffer[i] = get_random_char(props);
   }
   buffer[buffer_size] = '\0';
+}
+
+void freePtr(void **p) {
+  free(*p);
+  *p = NULL;
 }
